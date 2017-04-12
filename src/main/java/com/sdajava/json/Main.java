3d604d682654b56c49b07bc017d1685156351142
grovas <arczir@gmail.com>
2017-04-12 19:11:54 +0200

    2nd com

--- src/main/java/com/sdajava/json/Main.java
+++ src/main/java/com/sdajava/json/Main.java
@@ -1,8 +1,32 @@
 package com.sdajava.json;
 
+import org.json.simple.parser.JSONParser;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        JSONParser parser = new JSONParser();
+
+        try {
+            URL url = new URL("http://lukaszogan.com/data.json");
+            URLConnection connect = url.openConnection();
+            BufferedReader  in =
+                    new BufferedReader(
+                            new InputStreamReader(connect.getInputStream()));
+            in.lines().forEach(System.out::println);
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
